661a8a38baf3732a7a20bcd83029741bd578cace
Bilal Khan <mrbilalkhan2003@gmail.com>
2026-03-05 15:16:40 +0530

    Fix media folder and image issue

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4%$&!x_1o72&um8!ha*n^y=*349#1aodp_#z-*6@k_%qep*#uj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
